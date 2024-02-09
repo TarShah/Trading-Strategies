a0002ad5d0b92d325fc7d511163aeb04e93d83e2
TarShah <137076698+TarShah@users.noreply.github.com>
2024-02-09 20:38:33 +0530

    Update DMA++.cpp

--- DMA++.cpp
+++ DMA++.cpp
@@ -11,15 +11,205 @@ using namespace std;
 #define debug(...) 1
 #define imie(...) 1;
 #endif
-
-void solve(){
-	
-	
-	
+vector<string> dates;
+vector<double> price;
+string change_date_rep(string date){
+	string new_date = "";
+	new_date += date.substr(8,2);
+	new_date += "/";
+	new_date += date.substr(5,2);
+	new_date += "/";
+	new_date += date.substr(0,4); 
+	return new_date;
 }
-int main(){
-	ios::sync_with_stdio(false);
-	
+void get_dates(string symb){
+	string line;
+    ifstream file(symb + ".csv"); 
+    int targetColumnIndex = -1;
+    string targetColumnName = "DATE"; 
+	while(std::getline(file, line)){ 
+		if(targetColumnIndex == -1){
+			stringstream ss(line);
+			string columnName;
+			int columnIndex = 0;
+			while(std::getline(ss, columnName, ',')){
+				if(columnName == targetColumnName){
+					targetColumnIndex = columnIndex;
+					break;
+				}
+				columnIndex++;
+			}
+			assert(targetColumnIndex != -1);
+        }
+        else{
+			stringstream ss(line);
+			string cell;
+			int currentColumn = 0;
+			while(std::getline(ss, cell, ',')){ 
+				if(currentColumn == targetColumnIndex){  
+					dates.push_back(change_date_rep(cell)); 
+					break; 
+				}
+				currentColumn++;
+			}
+		}
+	}
+}
+void get_close(string symb){ 
+    string line;
+    ifstream file(symb + ".csv"); 
+    int targetColumnIndex = -1;
+    string targetColumnName = "CLOSE"; 
+	while(std::getline(file, line)){ 
+		if(targetColumnIndex == -1){
+			stringstream ss(line);
+			string columnName;
+			int columnIndex = 0;
+			while(std::getline(ss, columnName, ',')){
+				if(columnName == targetColumnName){
+					targetColumnIndex = columnIndex;
+					break;
+				}
+				columnIndex++;
+			}
+			assert(targetColumnIndex != -1);
+        }
+        else{
+			stringstream ss(line);
+			string cell;
+			int currentColumn = 0;
+			while(std::getline(ss, cell, ',')){ 
+				if(currentColumn == targetColumnIndex){  
+					price.push_back(stod(cell)); 
+					break; 
+				}
+				currentColumn++;
+			}
+		}
+	}
+}
+double get_SF(double SF,double ER,double c1, double c2){
+	double ratio = ((2*ER/(1+c2)) - 1) / ((2*ER/(1+c2)) + 1); 
+	ratio -= SF;
+	double new_SF = SF + c1*ratio;
+	return new_SF;
+}
+struct ord{
+	string type;
+	int index;
+};
+void solve(int argc,char* argv[]){
+	get_close(string(argv[1]));
+	get_dates(string(argv[1]));
+	reverse(all(dates));
+	reverse(all(price));
+	int n = stoi(argv[2]);
+	int x = stoi(argv[3]);
+	double p = stod(argv[4]);
+	int max_hold_days = stod(argv[5]);
+	double c1 = stod(argv[6]);
+	double c2 = stod(argv[7]);
+	string start_date = argv[8];
+	int start = 0;
+	for(int i=0;i<len(dates);i++){
+		if(dates[i] == start_date){ // to change this.
+			start = i;
+			break;
+		}
+	}
+	ll position = 0;
+	ll prev = start - (n-2);
+	double cash = 0;
+	double ER;
+	double SF = 0.5;
+	double AMA = price[start];
+	double sum_abs_change = 0;
+	for(int i = start - (n-2);i<=start;i++){
+		sum_abs_change += abs(price[start] - price[start-1]);
+	}
+	ofstream order("order_statistics.csv");
+	ofstream cashflow("daily_cashflow.csv");
+	cashflow<<dates[start]<<','<<cash<<endl;
+	queue<ord> q;
+	start++;
+	for(start;start<len(dates);start++){
+		bool buy = false;
+		bool sell = false;
+		bool held = false;
+		double price_change = abs(price[start] - price[start-n]);
+		sum_abs_change += abs(price[start] - price[start-1]);
+		ER = price_change/sum_abs_change;
+		SF = get_SF(SF,ER,c1,c2);
+		AMA = AMA + SF*(price[start] - AMA);
+		//cout<<AMA<<" "<<ER<<" "<<SF<<endl;
+		if(price[start] >= AMA*((double)(100+p) / 100)) buy = true;
+		if(price[start] <= AMA*((double)(100-p) / 100)) sell = true;
+		if(!q.empty() and start - q.front().index == max_hold_days){
+			held = true;
+		}
+		if(held){
+			bool held_buy = false;
+			bool held_sell = false;
+			if(q.front().type == "LONG") held_sell = true;
+			if(q.front().type == "SHORT") held_buy = true;
+			if(held_sell){
+				if(buy){
+					q.pop();
+					q.push(ord{"LONG",start});
+				}
+				else{
+					q.pop();
+					cash += price[start];
+					order<<dates[start]<<","<<"SELL"<<","<<1<<","<<price[start]<<endl;
+				}
+			}
+			if(held_buy){
+				if(sell){
+					q.pop();
+					q.push(ord{"SHORT",start});
+				}
+				else{
+					q.pop();
+					cash -= price[start];
+					order<<dates[start]<<","<<"BUY"<<","<<1<<","<<price[start]<<endl;
+				}
+			}
+			buy = false;
+			sell = false;
+		}
+		else{
+			if(buy){
+				if(position < x){
+					position++;
+					cash -= price[start];
+					order<<dates[start]<<","<<"BUY"<<","<<1<<","<<price[start]<<endl;
+					q.push(ord{"LONG",start});
+				}
+			}
+			if(sell){
+				if(position > -x){
+					position--;
+					cash += price[start];
+					order<<dates[start]<<","<<"SELL"<<","<<1<<","<<price[start]<<endl;
+					q.push(ord{"SHORT",start});
+				}
+			}
+		}
+		sum_abs_change -= abs(price[prev] - price[prev-1]);
+		prev++;
+		cashflow<<dates[start]<<','<<cash<<endl;
+	}
+	// Squaring off 
+	cash += price[len(price)-1]*position;
+	order.close();
+	cashflow.close();
+	// Writing the final pnl
+	ofstream pnl("final_pnl.txt");
+	pnl<<cash<<endl;
+	pnl.close();
+}
+int main(int argc, char*  argv[]){
+	solve(argc, argv);
 	
 	return 0;
 }
