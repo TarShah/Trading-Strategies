0288a6c3d36d66fcbc6b2750022a7c8af7f36ad8
TarShah <137076698+TarShah@users.noreply.github.com>
2024-02-09 16:24:26 +0530

    Update DMA.cpp

--- DMA.cpp
+++ DMA.cpp
@@ -11,15 +11,145 @@ using namespace std;
 #define debug(...) 1
 #define imie(...) 1;
 #endif
-
-void solve(){
-	
-	
-	
+vector<string> dates;
+vector<double> price;
+string change_date_rep(string date){
+	string new_date = "";
+	new_date += date.substr(8,2);
+	new_date += "/";
+	new_date += date.substr(5,2);
+	new_date += "/";
+	new_date += date.substr(0,4); 
+	return new_date;
 }
-int main(){
-	ios::sync_with_stdio(false);
-	
+void get_dates(string symb){
+	string line;
+    ifstream file(symb + ".csv"); 
+    int targetColumnIndex = -1;
+    string targetColumnName = "DATE"; 
+	while(std::getline(file, line)){ 
+		if(targetColumnIndex == -1){
+			stringstream ss(line);
+			string columnName;
+			int columnIndex = 0;
+			while(std::getline(ss, columnName, ',')){
+				if(columnName == targetColumnName){
+					targetColumnIndex = columnIndex;
+					break;
+				}
+				columnIndex++;
+			}
+			assert(targetColumnIndex != -1);
+        }
+        else{
+			stringstream ss(line);
+			string cell;
+			int currentColumn = 0;
+			while(std::getline(ss, cell, ',')){ 
+				if(currentColumn == targetColumnIndex){  
+					dates.push_back(change_date_rep(cell)); 
+					break; 
+				}
+				currentColumn++;
+			}
+		}
+	}
+}
+void get_close(string symb){ 
+    string line;
+    ifstream file(symb + ".csv"); 
+    int targetColumnIndex = -1;
+    string targetColumnName = "CLOSE"; 
+	while(std::getline(file, line)){ 
+		if(targetColumnIndex == -1){
+			stringstream ss(line);
+			string columnName;
+			int columnIndex = 0;
+			while(std::getline(ss, columnName, ',')){
+				if(columnName == targetColumnName){
+					targetColumnIndex = columnIndex;
+					break;
+				}
+				columnIndex++;
+			}
+			assert(targetColumnIndex != -1);
+        }
+        else{
+			stringstream ss(line);
+			string cell;
+			int currentColumn = 0;
+			while(std::getline(ss, cell, ',')){ 
+				if(currentColumn == targetColumnIndex){  
+					price.push_back(stod(cell)); 
+					break; 
+				}
+				currentColumn++;
+			}
+		}
+	}
+}
+void solve(int argc,char* argv[]){
+	get_close(string(argv[1]));
+	get_dates(string(argv[1]));
+	reverse(all(dates));
+	reverse(all(price));
+	int n = stoi(argv[2]);
+	int x = stoi(argv[3]);
+	double p = stod(argv[4]);
+	string start_date = argv[5];
+	int start = 0;
+	for(int i=0;i<len(dates);i++){
+		if(dates[i] == start_date){
+			start = i;
+			break;
+		}
+	}
+	double sum = 0;
+	double sum_of_squares = 0;
+	double prev = start - (n-1);
+	for(int i = start - (n-1);i<start;i++){
+		sum += price[i];
+		sum_of_squares += price[i]*price[i];
+	}
+	ll position = 0;
+	double cash = 0;
+	ofstream order("order_statistics.csv");
+	ofstream cashflow("daily_cashflow.csv");
+	for(start;start<len(dates);start++){
+		sum += price[start];
+		sum_of_squares += price[start]*price[start];
+		double DMA = (double)(sum/(double)n);
+		double SD = sqrt((double)(sum_of_squares/(double)n) - (double)DMA*DMA);
+		if(price[start] - DMA >= p*SD){
+			if(position < x){
+				position++;
+				cash -= price[start];
+				order<<dates[start]<<","<<"BUY"<<","<<1<<","<<price[start]<<endl;
+			}
+		}
+		if(DMA - price[start] >= p*SD){
+			if(position > -x){
+				position--;
+				cash += price[start];
+				order<<dates[start]<<","<<"SELL"<<","<<1<<","<<price[start]<<endl;
+			}
+		}
+		sum -= price[prev];
+		sum_of_squares -= price[prev]*price[prev];
+		prev++;
+		cashflow<<dates[start]<<','<<cash<<endl;
+	}
+	// Squaring off 
+	cash += price[len(price)-1]*position;
+	order.close();
+	cashflow.close();
+	// Writing the final pnl
+	ofstream pnl("final_pnl.txt");
+	pnl<<cash<<endl;
+	pnl.close();
+}
+int main(int argc, char*  argv[]){
+	solve(argc, argv);
 	
 	return 0;
 }
