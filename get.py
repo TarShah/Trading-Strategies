9a4124ab27fca12e025fa830a8e8c392e207d564
rathiaditya1 <140103118+rathiaditya1@users.noreply.github.com>
2024-02-12 16:27:55 +0530

    Update get.py
    minor changes

--- get.py
+++ get.py
@@ -3,14 +3,16 @@ from datetime import datetime
 from jugaad_data.nse import stock_df
 
 #to change year to current year -1 
-def convert_to_earlier_year(date_str):
+def convert_to_earlier_year(date_str,n):
     date_obj = datetime.strptime(date_str, '%d/%m/%Y')
-    earlier_year_date = date_obj - timedelta(days=366)  # Not perfectly accurate but a simple approximation
+    earlier_year_date = date_obj - timedelta(days=n)  
     return earlier_year_date.strftime('%d/%m/%Y')
 
 symb = sys.argv[1]
-sdt = convert_to_earlier_year(sys.argv[2])
+n = sys.argv[4]
+sdt = convert_to_earlier_year(sys.argv[2],n)
 edt = sys.argv[3]
+
 df = stock_df(symbol=symb, from_date=datetime.strptime(sdt,"%d/%m/%Y"),to_date=datetime.strptime(edt,"%d/%m/%Y"), series="EQ",)
 cols = ["DATE","OPEN","CLOSE","HIGH","LOW","LTP","VOLUME","VALUE","NO OF TRADES"]
 new_df = df[cols].reset_index(drop=True)
